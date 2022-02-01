28af72f06aab971e57caca4170fdfb45c3ff7f31
atouts <atouts@sheridancollege.ca>
2022-02-01 13:49:43 -0500

    Added Codes

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,4 +1,9 @@
 /*
+Safa Atout
+991620668
+*/
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,5 +20,7 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        Card newCards = new Card ("CardClass", 2);
+        newCards.setSuit("Angles");
     }
 }
